02eadad1cd3df9b70b7bc23611d88f5041aa570b
Andre <cruzandrecal@gmail.com>
2022-10-14 15:54:50 -0300

    Parte 4

---
+++ src/Pages/SignIn.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { rest } from 'msw'
+import { SignIn } from './SignIn'
+
+export default {
+    title: 'Pages/Sign in',
+    component: SignIn,
+    args: {},
+    argTypes: {},
+    parameters: {
+        msw: {
+            handlers: [
+                rest.post('/sessions', (req, res, ctx) => {
+                    return res(ctx.json({
+                        message: 'Login realizado!'
+                    }))
+                })
+            ]
+        }
+    }
+} as Meta
+
+export const Default: StoryObj = {
+    //testes automáticos
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+
+        userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'andre@arpanet.com')
+        userEvent.type(canvas.getByPlaceholderText('*********'), '1234567890')
+
+        userEvent.click(canvas.getByRole('button'))
+
+        await waitFor(() => {
+            return expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
+        })
+    }
+}
